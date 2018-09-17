c4117a58efd1abb86ba4b38e5ecf8f69a0dc3c43
Halyson Mendonça <hmendonca@apassos.com.br>
2018-09-17 17:32:27 -0400

    Primeiro Enviar

---
+++ Aplicacao.Dados/Repository/ProdutoRepositorio.cs
@@ -0,0 +1,43 @@
+﻿using Aplicacao.Dados.Repository.Interface;
+using Aplicacao.Loja.Loja;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplicacao.Dados.Repository
+{
+    public class ProdutoRepositorio : IRepositorio<Produto>
+    {
+        public void Add(Produto entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Produto entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(Produto entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Produto> FindBy(System.Linq.Expressions.Expression<Func<Produto, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Produto> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
